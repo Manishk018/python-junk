bdf9bd43a6d02ec490db1af8ab84af224246a0e2
Manishk018 <manish1259@gmail.com>
2023-08-08 01:23:38 +0530

    commit1: Monty hall program

---
+++ monty.py
@@ -0,0 +1,103 @@
+import random
+
+class RandomOne:
+    def isWin(self, selected, removed, d1, d2, d3):
+       if removed==0:
+           if selected==1:
+               if d2==1:
+                   return True
+               else:
+                    return False
+           else:
+               if d3==1:
+                   return True
+               else:
+                   return False
+               
+       elif removed==1:
+           if selected==2:
+               if d3==1:
+                   return True
+               else:
+                   return False
+           else:
+               if d1==1:
+                   return True
+               else:
+                   return False
+               
+       else:
+           if selected==0:
+               if d1==1:
+                   return True
+               else:
+                   return False
+           else:
+               if d2==1:
+                   return True
+               else:
+                   return False
+           
+
+    def get_three_values(self):
+        # Generate a list with two 0s and one 1 randomly arranged
+        values = [0, 0, 1]
+        random.shuffle(values)
+        
+        # Return the three variables
+        return values[0], values[1], values[2]
+    
+    def remove1door(self,selected,d1,d2,d3):
+        if selected==0:
+            if d2==1:
+                return 2
+            else:
+                return 1
+        elif selected==1:
+            if d3==1:
+                return 0
+            else:
+                return 2
+        else:
+            if d1==1:
+                return 1
+            else:
+                return 0
+
+# Usage example:
+random_one = RandomOne()
+
+win=0
+loose=0
+
+switch=0
+switch=int(input('Do you want to run simulation door switch?(0/1) : '))
+itr=int(input('Run simulation for..? : '))
+
+for i in range(itr):
+    d1, d2, d3 = random_one.get_three_values()
+    # print(d1, d2, d3)
+    selected=random.randint(0,2)
+    # print('Selected:',selected)
+    removed=random_one.remove1door(selected,d1,d2,d3)
+    # print('removed:',removed)
+    res=random_one.isWin(selected,removed,d1,d2,d3)
+    # print('win?:',res)
+    if switch==0:
+        if res==True:
+            win+=1
+        else:
+            loose+=1
+    else:
+         if res==True:
+            loose+=1
+         else:
+            win+=1
+    # print('running simulation..')
+
+if switch==0:  
+    print('When there was no door switch done!')
+else:
+    print('When there was door switch done!')
+print('win:      ',win)
+print('loose:    ',loose)
